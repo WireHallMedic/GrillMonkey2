b9fac82cca70e93a7e5d22cfc01848d436bb428c
WireHallMedic <michaelwidler@yahoo.com>
2022-03-01 12:18:16 -0800

    Add images and imports from WidlerSuite.

---
+++ IntDeck.java
@@ -0,0 +1,97 @@
+/*
+
+An implementation for bagging; give it a range of ints, tell it how many
+copies of each you want, and it makes a deck of them to be popped one at a time.
+
+Code adapted from WidlerSuite, a project also by this author.
+
+*/
+
+package GrillMonkey2;
+
+import java.util.*;
+
+public class IntDeck
+{
+   private Vector<Integer> deck;
+   private int _copies;
+   private int[] _values;
+   private SquirrelRNG rng;
+   
+   // constructor to make n copies of a passed list of ints
+   public IntDeck(int[] values, int copies, int seed)
+   {
+      rng = new SquirrelRNG(seed);
+      deck = null;
+      _values = new int[values.length];
+      for(int i = 0; i < values.length; i++)
+         _values[i] = values[i];
+      _copies = copies;
+      shuffle();
+   }
+   
+   // constructor with time as default seed
+   public IntDeck(int[] values, int copies)
+   {
+      this(values, copies, (int)System.currentTimeMillis());
+   }
+   
+   // deck of a single copy of the passed values
+   public IntDeck(int[] values)
+   {
+      this(values, 1);
+   }
+   
+   // shuffle the deck (full set of values) with seed
+   public void shuffle(int seed)
+   {
+      rng.setSeed(seed);
+      shuffle();
+   }
+   
+   // shuffle the deck without reseeding
+   public void shuffle()
+   {
+      Vector<Integer> orderedList = new Vector<Integer>();
+      deck = new Vector<Integer>();
+      
+      // create all the elements in one list
+      for(int i = 0; i < _values.length; i++)
+      for(int j = 0; j < _copies; j++)
+         orderedList.add(_values[i]);
+      
+      // randomly add them to a different list
+      int index = 0;
+      while(orderedList.size() > 0)
+      {
+         index = (int)(orderedList.size() * rng.nextDouble());
+         deck.add(orderedList.elementAt(index));
+         orderedList.removeElementAt(index);
+      }
+   }
+   
+   // pop the top entry
+   public int pop()
+   {
+      if(deck.size() == 0)
+         shuffle();
+      int val = deck.elementAt(0).intValue();
+      deck.removeElementAt(0);
+      return val;
+   }
+   
+   // testing/demo method
+   public static void main(String[] args)
+   {
+      int[] list = {1, 2, 3, 4, 5};
+      IntDeck d = new IntDeck(list, 2);
+      for(int i = 0; i < 3; i++)
+      {
+         for(int j = 0; j < 10; j++)
+         {
+            System.out.print(d.pop() + " ");
+         }
+         System.out.println();
+      }
+   }
+}
---
+++ SquirrelRNG.java
@@ -0,0 +1,146 @@
+/*
+
+Based (heavily) on a GDC talk by Squirrel Eiserloh.
+Essentially a hashing function posing as an RNG. Fast, tiny, good-quality output.
+A given instance is not threadsafe.
+
+Random indexing, sequential indexing, fast reseeding, consistent speed, memory 
+footprint rounds to zero.
+
+Code copied from WidlerSuite, a project also by this author.
+
+*/
+
+package GrillMonkey2;
+
+public class SquirrelRNG
+{
+   private static final int BIT_NOISE_ONE =   0xB5297A4D;
+   private static final int BIT_NOISE_TWO =   0x68E31DA4;
+   private static final int BIT_NOISE_THREE = 0x1B56C4E9;
+   private static final int BIG_PRIME_NUMBER = 198491317;
+   private int _seed = 0;
+   private int lastIndexed = 0;
+   
+   // seeded constructor
+   public SquirrelRNG(int seed)
+   {
+      setSeed(seed);
+      lastIndexed = 0;
+   }
+   
+   // unseeded constructor (seeds with current time)
+   public SquirrelRNG()
+   {
+      this((int)System.currentTimeMillis());
+   }
+   
+   // set seed
+   public void setSeed(int s)
+   {
+      _seed = s;
+   }
+   
+   // set seed and get int value at a position
+   public int sampleInt(int position, int seed)
+   {
+      setSeed(seed);
+      return sampleInt(position);
+   }
+   
+   // get next double value, based on last value sampled
+   public double nextDouble()
+   {
+      return sampleDouble(++lastIndexed);
+   }
+   
+   // get next int value, based on last value sampled
+   public int nextInt()
+   {
+      return sampleInt(++lastIndexed);
+   }
+   
+   // get value at position offset from existing seed (which may be zero)
+   public int sampleInt(int position)
+   {
+      lastIndexed = position;
+      int val = Math.abs(position + _seed);
+      val *= BIT_NOISE_ONE;
+      val = val ^ (val >>> 8);
+      val += BIT_NOISE_TWO;
+      val = val ^ (val << 8);
+      val *= BIT_NOISE_THREE;
+      val = val ^ (val >>> 8);
+      val = Math.abs(val);
+      return val;
+   }
+   
+   // int to double conversion method
+   private static double toDouble(int val)
+   {
+      return (double)val / (double)Integer.MAX_VALUE;
+   }
+   
+   // get value at position offset from existing seed (which may be zero)
+   public double sampleDouble(int position)
+   {
+      return toDouble(sampleInt(position));
+   }
+   
+   // get int value at 2D position
+   public int sample2DInt(int x, int y)
+   {
+      return sampleInt(x + (BIG_PRIME_NUMBER * y));
+   }
+   
+   // get double value at 2D position
+   public double sample2DDouble(int x, int y)
+   {
+      return toDouble(sample2DInt(x, y));
+   }
+   
+   // set seed and get int value at 2D position
+   public int sample2DInt(int x, int y, int seed)
+   {
+      setSeed(seed);
+      return sample2DInt(x, y);
+   }
+   
+   // set seed and get double value at 2D position
+   public double sample2DDouble(int x, int y, int seed)
+   {
+      return toDouble(sample2DInt(x, y, seed));
+   }
+   
+   // testing/demo method
+   public static void main(String[] args)
+   {
+      SquirrelRNG rng = new SquirrelRNG(0);
+      double sum = 0.0;
+      double val = 0.0;
+      int iters = 10000000;
+      int[] outArr = new int[10];
+      for(int i = 0; i < iters; i++)
+      {
+         //System.out.println(String.format("%f.5", SquirrelRNG.sample(i)));
+         val = rng.sampleDouble(i);
+         sum += val;
+         outArr[(int)(val * 10.0)]++;
+      }
+      System.out.println(String.format("Average = %.5f", sum / (double)iters));
+      for(int i = 0; i < 10; i++)
+         System.out.println(String.format("Bin #%d: %7d", i, outArr[i]));
+      
+      for(int i = 0; i < 10; i++)
+         System.out.println(rng.nextDouble() + "");
+      
+      for(int x = 0; x < 5; x++)
+         {
+         for(int y = 0; y < 5; y++)
+         {
+            System.out.print(String.format("%.3f ", rng.sample2DDouble(x, y)));
+         }
+         System.out.println();
+      }
+   }
+}
