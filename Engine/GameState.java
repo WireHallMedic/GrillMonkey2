8470079ba987f08a52ccd3f9ee9657a9fafeb56d
WireHallMedic <michaelwidler@yahoo.com>
2022-03-08 11:04:05 -0800

    Rename GM[...] to GM2[...].

--- Engine/GameState.java
+++ Engine/GameState.java
@@ -1,6 +1,13 @@
 package GrillMonkey2.Engine;
 
-public class GameState
+public class GameState implements GM2Constants
 {
-
+   private Tile[][] board;
+   
+   public GameState()
+   {
+      board = new Tile[BOARD_WIDTH][BOARD_HEIGHT];
+   }
+   
+   
 }
